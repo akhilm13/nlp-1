965db42dcc29c30d6edf6bf241be99b08a850ecc
akhil <akhil.hardys@gmail.com>
2018-04-20 15:42:54 +0200

    Done Part 1

--- subject_extraction/subject_extraction.py
+++ subject_extraction/subject_extraction.py
@@ -1,5 +1,6 @@
 from trigram_tagger import SubjectTrigramTagger
 from bs4 import BeautifulSoup
+import sys
 import requests
 import re
 import pickle
@@ -17,6 +18,7 @@ def download_document(url):
     """Downloads document using BeautifulSoup, extracts the subject and all
     text stored in paragraph tags
     """
+    print(url)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     title = soup.find('title').get_text()
@@ -72,42 +74,6 @@ def extract_subject(document):
     print (subject_nouns)
     return subject_nouns[0]
 
-def trained_tagger(existing=False):
-    """Returns a trained trigram tagger
-
-    existing : set to True if already trained tagger has been pickled
-    """
-    if existing:
-        trigram_tagger = pickle.load(open('trained_tagger.pkl', 'rb'))
-        return trigram_tagger
-
-    # Aggregate trained sentences for N-Gram Taggers
-    train_sents = nltk.corpus.brown.tagged_sents()
-    train_sents += nltk.corpus.conll2000.tagged_sents()
-    train_sents += nltk.corpus.treebank.tagged_sents()
-
-    # Create instance of SubjectTrigramTagger and persist instance of it
-    trigram_tagger = SubjectTrigramTagger(train_sents)
-    pickle.dump(trigram_tagger, open('trained_tagger.pkl', 'wb'))
-
-    return trigram_tagger
-
-def tag_sentences(subject, document):
-    """Returns tagged sentences using POS tagging"""
-    trigram_tagger = trained_tagger(existing=True)
-
-    # Tokenize Sentences and words
-    sentences = tokenize_sentences(document)
-    merge_multi_word_subject(sentences, subject)
-
-    # Filter out sentences where subject is not present
-    sentences = [sentence for sentence in sentences if subject in
-                [word.lower() for word in sentence]]
-
-    # Tag each sentence
-    tagged_sents = [trigram_tagger.tag(sent) for sent in sentences]
-    return tagged_sents
-
 def merge_multi_word_subject(sentences, subject):
     """Merges multi word subjects into one single token
     ex. [('steve', 'NN', ('jobs', 'NN')] -> [('steve jobs', 'NN')]
@@ -127,43 +93,14 @@ def merge_multi_word_subject(sentences, subject):
                     sentences[i][j] = subject
     return sentences
 
-def get_svo(sentence, subject):
-    """Returns a dictionary containing:
-
-    subject : the subject determined earlier
-    action : the action verb of particular related to the subject
-    object : the object the action is referring to
-    phrase : list of token, tag pairs for that lie within the indexes of
-                the variables above
-    """
-    subject_idx = next((i for i, v in enumerate(sentence)
-                    if v[0].lower() == subject), None)
-    data = {'subject': subject}
-    for i in range(subject_idx, len(sentence)):
-        found_action = False
-        for j, (token, tag) in enumerate(sentence[i+1:]):
-            if tag in VERBS:
-                data['action'] = token
-                found_action = True
-            if tag in NOUNS and found_action == True:
-                data['object'] = token
-                data['phrase'] = sentence[i: i+j+2]
-                return data
-    return {}
-
-if __name__ == '__main__':
-    url = 'https://www.usatoday.com/story/news/world/2018/02/19/donald-trump-jr-s-trip-india-could-mix-business-and-u-s-foreign-policy/352127002/'
+def get_subject(link):
+    url = link
+#'https://www.usatoday.com/story/news/world/2018/02/19/donald-trump-jr-s-trip-india-could-mix-business-and-u-s-foreign-policy/352127002/'
     document = download_document(url)
     # document = pickle.load(open('document.pkl', 'rb'))
-    print (document)
+
+    #print (document)
     document = clean_document(document)
     subject = extract_subject(document)
-    print (subject)
+    return subject;
 
-#    tagged_sents = tag_sentences(subject, document)
-
- #   svos = [get_svo(sentence, subject)
-  #                      for sentence in tagged_sents]
-   # for svo in svos:
-    #    if svo:
-     #       print svo
